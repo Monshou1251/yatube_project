7c760c9a058c13a6cd1b57826018b7f399d62d05
Григорий Юрченко <grigory.urchenko@gmail.com>
2022-05-16 23:46:29 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница. Её я сделал сам, прям как hackerman')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Страница с постами. А вот и '
+                        f'неведомое сообщение переданое ранее - {slug}')
+
+
